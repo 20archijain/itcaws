d4978c43baa36c65ae1af8966ad86f6e335b49b4
Pranjal Srivastava <pns29397.com>
2025-10-13 17:07:52 +0530

    changes in UOB Report filter

--- itcph2/dist/services/modules/downloadReports/billCutReport.php
+++ itcph2/dist/services/modules/downloadReports/billCutReport.php
@@ -18,6 +18,24 @@ if (!isEmptyString($requestAction)) {
         case $ACTION_LIST['GET_DOWNLOAD_DATA']:
             $download->getDownloadData();
             break;
+        case $ACTION_LIST['GET_BRANCH']:
+            $download->getBranch();
+            break;
+        case $ACTION_LIST['GET_CIRCLE']:
+            $download->getCircle();
+            break;
+        case $ACTION_LIST['GET_SECTION']:
+            $download->getSection();
+            break;
+        case $ACTION_LIST['GET_WD_CODE']:
+            $download->getWDCode();
+            break;
+        case $ACTION_LIST['GET_TEAM_TYPE_LIST']:
+            $download->getTeamType();
+            break;
+        case $ACTION_LIST['GET_TEAM_LIST']:
+            $download->getTeamList();
+            break;
         default:
             $arrMessage = responseMessage(array($INVALID_ACTION));
             echo json_encode($arrMessage);
--- itcph2/src/app/main/components/dowload-bill-cut/dowload-bill-cut.component.ts
+++ itcph2/src/app/main/components/dowload-bill-cut/dowload-bill-cut.component.ts
@@ -8,7 +8,7 @@ import { environment } from 'src/environments/environment';
 import { REQUEST_STATUS, STATIC_MODULES } from 'src/app/app.constants';
 import { Functions } from 'src/app/core/utils/functions.list';
 import { COMMON_VALIDATORS } from 'src/app/core/validators/validations.list';
-import { DropdownList, GetDownloadBillCutResponse, GetDownloadFileDetails } from 'src/app/core/interfaces/http-response.interface';
+import { DashboardData, DropdownList, GetDownloadBillCutResponse, GetDownloadFileDetails } from 'src/app/core/interfaces/http-response.interface';
 import { LoaderService } from 'src/app/core/services/loader.service';
 
 @Component({
@@ -18,8 +18,15 @@ export class DowloadBillCutComponent implements OnDestroy, OnInit {
   private subscription: Subscription[] = [];
   group: UntypedFormGroup;
   branchOptions: DropdownList[] = [];
-  teamTypeOptions: DropdownList[] = [];
   productOptions: DropdownList[] = [];
+  teamOptions: DropdownList[] = [];
+  teamTypeOptions: DropdownList[] = [];
+  circleOptions: DropdownList[] = [];
+  sectionOptions: DropdownList[] = [];
+  wdCodeOptions: DropdownList[] = [];
+  districtOptions: DropdownList[] = [];
+  wdMarketOptions: DropdownList[] = [];
+  wdPopGroupOptions: DropdownList[] = [];
   errorMessages = {
     branch: COMMON_VALIDATORS.messages.requiredOnly('Branch'),
     dateFrom: COMMON_VALIDATORS.messages.requiredOnly('From'),
@@ -35,7 +42,14 @@ export class DowloadBillCutComponent implements OnDestroy, OnInit {
       branch: ['', COMMON_VALIDATORS.validators.requiredOnly],
       dateFrom: ['', COMMON_VALIDATORS.validators.date],
       dateTo: ['', COMMON_VALIDATORS.validators.date],
-      teamType: [''],
+      circle: [''],
+      section: [''],
+      wdCode: [''],
+      dsType: [''],
+      dsName: [''],
+      district: [''],
+      wdMarket: [''],
+      wdPopGroup: [''],
       product: ['']
     });
 
@@ -47,7 +61,15 @@ export class DowloadBillCutComponent implements OnDestroy, OnInit {
         )
         .subscribe(resp => {
           if (resp && resp.status === REQUEST_STATUS.SUCCESS) {
+            this.districtOptions = resp.data.districtList;
             this.branchOptions = resp.data.branchList;
+            this.circleOptions = resp.data.circleList;
+            this.sectionOptions = resp.data.sectionList;
+            this.wdCodeOptions = resp.data.wdCodeList;
+            this.teamOptions = resp.data.teamList;
+            this.teamTypeOptions = resp.data.teamType;
+            this.wdMarketOptions = resp.data.wdMarketList;
+            this.wdPopGroupOptions = resp.data.wdPopGroupList;
           }
         })
     );
@@ -79,46 +101,193 @@ export class DowloadBillCutComponent implements OnDestroy, OnInit {
     }
   }
 
-  getTeamsType() {
-    this.teamTypeValue = null;
+    getBranch() {
+      this.branchValue = null;
+      this.circleValue = null;
+      this.sectionValue = null;
+      this.wdCodeValue = null;
+      this.dsTypeValue = null;
+      this.dsNameValue = null;
+      this.wdMarketValue = null;
+      this.wdPopGroupValue = null;
+      this.loaderService.startLoader();
+      this.subscription.push(
+        this.formService.customActionCall<DashboardData>(STATIC_MODULES.custom.getBranch, { district: this.group.get('district').value }, null, environment.viewVanDsDataUrl)
+          .pipe(finalize(() => this.loaderService.stopLoader()))
+          .subscribe(resp => {
+            if (resp && resp.status === REQUEST_STATUS.SUCCESS) {
+              this.branchOptions = resp.data.branchList;
+              this.circleOptions = resp.data.circleList;
+              this.sectionOptions = resp.data.sectionList;
+              this.wdCodeOptions = resp.data.wdCodeList;
+              this.teamOptions = resp.data.teamList;
+              this.teamTypeOptions = resp.data.teamType;
+              this.wdMarketOptions = resp.data.wdMarketList;
+              this.wdPopGroupOptions = resp.data.wdPopGroupList;
+            }
+          })
+      );
+    }
 
-    this.loaderService.startLoader();
-    this.subscription.push(
-      this.formService.customActionCall<GetDownloadBillCutResponse>(STATIC_MODULES.custom.getTeamsTypeList, { branch: this.group.get('branch').value }, null, this.url)
-        .pipe(
-          finalize(() => this.loaderService.stopLoader())
-        )
-        .subscribe(resp => {
-          if (resp && resp.status === REQUEST_STATUS.SUCCESS) {
-            this.teamTypeOptions = resp.data.teamType;
-            this.productOptions = resp.data.productList;
-          }
-        })
-    );
-  }
+    getCircle() {
+      this.circleValue = null;
+      this.sectionValue = null;
+      this.wdCodeValue = null;
+      this.dsTypeValue = null;
+      this.dsNameValue = null;
+      this.wdMarketValue = null;
+      this.wdPopGroupValue = null;
+      this.productValue = null;
+      this.loaderService.startLoader();
+      this.subscription.push(
+        this.formService.customActionCall<DashboardData>(STATIC_MODULES.custom.getCircle, { branch: this.group.get('branch').value }, null, environment.viewVanDsDataUrl)
+          .pipe(finalize(() => this.loaderService.stopLoader()))
+          .subscribe(resp => {
+            if (resp && resp.status === REQUEST_STATUS.SUCCESS) {
+              this.circleOptions = resp.data.circleList;
+              this.sectionOptions = resp.data.sectionList;
+              this.wdCodeOptions = resp.data.wdCodeList;
+              this.teamOptions = resp.data.teamList;
+              this.teamTypeOptions = resp.data.teamType;
+              this.wdMarketOptions = resp.data.wdMarketList;
+              this.wdPopGroupOptions = resp.data.wdPopGroupList;
+              this.productOptions = resp.data.productList;
+            }
+          })
+      );
+    }
 
-  getProducts() {
-    this.group.get('product').setValue('')
-    this.loaderService.startLoader();
-    this.subscription.push(
-      this.formService.customActionCall<GetDownloadBillCutResponse>('get_product_list', { type: this.group.get('teamType').value, branch: this.group.get('branch').value }, null, this.url)
-        .pipe(
-          finalize(() => this.loaderService.stopLoader())
-        )
-        .subscribe(resp => {
-          if (resp && resp.status === REQUEST_STATUS.SUCCESS) {
-            this.productOptions = resp.data.productList;
-          }
-        })
-    );
-  }
+    getSection() {
+      this.sectionValue = null;
+      this.wdCodeValue = null;
+      this.dsTypeValue = null;
+      this.dsNameValue = null;
+      this.wdMarketValue = null;
+      this.wdPopGroupValue = null;
+      this.loaderService.startLoader();
+      this.subscription.push(
+        this.formService.customActionCall<DashboardData>(STATIC_MODULES.custom.getSection, { branch: this.group.get('branch').value, circle: this.group.get('circle').value }, null, environment.viewVanDsDataUrl)
+          .pipe(finalize(() => this.loaderService.stopLoader()))
+          .subscribe(resp => {
+            if (resp && resp.status === REQUEST_STATUS.SUCCESS) {
+              this.sectionOptions = resp.data.sectionList;
+              this.wdCodeOptions = resp.data.wdCodeList;
+              this.teamOptions = resp.data.teamList;
+              this.teamTypeOptions = resp.data.teamType;
+              this.wdMarketOptions = resp.data.wdMarketList;
+              this.wdPopGroupOptions = resp.data.wdPopGroupList;
+            }
+          })
+      );
+    }
+
+    getWDCode() {
+      this.wdCodeValue = null;
+      this.dsTypeValue = null;
+      this.dsNameValue = null;
+      this.wdMarketValue = null;
+      this.wdPopGroupValue = null;
+      this.loaderService.startLoader();
+      this.subscription.push(
+        this.formService.customActionCall<DashboardData>(STATIC_MODULES.custom.getWDList, { branch: this.group.get('branch').value, circle: this.group.get('circle').value, section: this.group.get('section').value }, null, environment.viewVanDsDataUrl)
+          .pipe(finalize(() => this.loaderService.stopLoader()))
+          .subscribe(resp => {
+            if (resp && resp.status === REQUEST_STATUS.SUCCESS) {
+              this.wdCodeOptions = resp.data.wdCodeList;
+              this.teamOptions = resp.data.teamList;
+              this.teamTypeOptions = resp.data.teamType;
+              this.wdMarketOptions = resp.data.wdMarketList;
+              this.wdPopGroupOptions = resp.data.wdPopGroupList;
+            }
+          })
+      );
+    }
+
+    getTeamsType() {
+      this.dsTypeValue = null;
+      this.dsNameValue = null;
+      this.loaderService.startLoader();
+      this.subscription.push(
+        this.formService.customActionCall<DashboardData>(STATIC_MODULES.custom.getTeamsTypeList, { branch: this.group.get('branch').value, circle: this.group.get('circle').value, section: this.group.get('section').value, wdCode: this.group.get('wdCode').value }, null, environment.viewVanDsDataUrl)
+          .pipe(finalize(() => this.loaderService.stopLoader()))
+          .subscribe(resp => {
+            if (resp && resp.status === REQUEST_STATUS.SUCCESS) {
+              this.teamTypeOptions = resp.data.teamType;
+              this.teamOptions = resp.data.teamList;
+            }
+          })
+      );
+    }
+
+    getTeamsName() {
+      this.dsNameValue = null;
+      this.loaderService.startLoader();
+      this.subscription.push(
+        this.formService.customActionCall<DashboardData>(STATIC_MODULES.custom.getTeamsList, { branch: this.group.get('branch').value, circle: this.group.get('circle').value, section: this.group.get('section').value, wdCode: this.group.get('wdCode').value, dsType: this.group.get('dsType').value }, null, environment.viewVanDsDataUrl)
+          .pipe(finalize(() => this.loaderService.stopLoader()))
+          .subscribe(resp => {
+            if (resp && resp.status === REQUEST_STATUS.SUCCESS) {
+              this.teamOptions = resp.data.teamList;
+            }
+          })
+      );
+    }
+
+  // getProducts() {
+  //   this.group.get('product').setValue('')
+  //   this.loaderService.startLoader();
+  //   this.subscription.push(
+  //     this.formService.customActionCall<GetDownloadBillCutResponse>('get_product_list', { type: this.group.get('teamType').value, branch: this.group.get('branch').value }, null, this.url)
+  //       .pipe(
+  //         finalize(() => this.loaderService.stopLoader())
+  //       )
+  //       .subscribe(resp => {
+  //         if (resp && resp.status === REQUEST_STATUS.SUCCESS) {
+  //           this.productOptions = resp.data.productList;
+  //         }
+  //       })
+  //   );
+  // }
 
   get branchValue() {
     return this.group && this.group.get('branch').value;
   }
-  set teamTypeValue(value: string) {
+
+  set branchValue(value: string) {
+    this.branchOptions = [];
+    this.group.get('branch').setValue(value);
+  }
+  set circleValue(value: string) {
+    this.circleOptions = [];
+    this.group.get('circle').setValue(value);
+  }
+  set sectionValue(value: string) {
+    this.sectionOptions = [];
+    this.group.get('section').setValue(value);
+  }
+  set wdCodeValue(value: string) {
+    this.wdCodeOptions = [];
+    this.group.get('wdCode').setValue(value);
+  }
+  set dsTypeValue(value: string) {
     this.teamTypeOptions = [];
-    this.group.get('teamType').setValue(value);
+    this.group.get('dsType').setValue(value);
+  }
+  set dsNameValue(value: string) {
+    this.teamOptions = [];
+    this.group.get('dsName').setValue(value);
+  }
+  set wdMarketValue(value: string) {
+    this.wdMarketOptions = [];
+    this.group.get('wdMarket').setValue(value);
+  }
+  set wdPopGroupValue(value: string) {
+    this.wdPopGroupOptions = [];
+    this.group.get('wdPopGroup').setValue(value);
+  }
+  set productValue(value: string) {
+    this.productOptions = [];
+    this.group.get('product').setValue(value);
   }
 
 }
