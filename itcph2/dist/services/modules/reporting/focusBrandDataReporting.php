672b501f1a96d97a914e0da0101b3b79b7e2714a
Pranjal Srivastava <pns29397.com>
2025-12-09 14:22:58 +0530

    add new focus brand reporting

---
+++ itcph2/dist/services/modules/reporting/focusBrandDataReporting.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once $include_path . "defined_index.php";
+require_once $include_path . "class/reporting/FocusBrandDataReporting.php";
+
+if (!isEmptyString($requestAction)) {
+    $team = new FocusBrandDataReporting($dbConn, $requestData, $arrAccessInfo, $iUserId);
+    switch ($requestAction) {
+        case $ACTION_LIST['GET_DATA']:
+            $team->getViewSKUData();
+            break;
+        // case $ACTION_LIST['GET_LIST']:
+        //     $team->viewSKUData();
+        //     break;
+        case $ACTION_LIST['GET_DOWNLOAD_DATA']:
+            $team->downloadMasterData();
+            break;
+        default:
+            $arrMessage = responseMessage(array($INVALID_ACTION));
+            echo json_encode($arrMessage);
+    }
+} else {
+    $arrMessage = responseMessage(array($NO_ACTION_FOUND));
+    echo json_encode($arrMessage);
+}
